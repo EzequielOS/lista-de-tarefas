1a7c43c8987dccb190db2f67b29c7363c55de692
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-11-22 16:08:44 -0200

    2nd commit: Grupo de Tarefas 2 (O usuario insere Tarefas)

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function enter() {
-	if (event.keyCode==13){
-        alert("foi enter");
+    var textoDigitado = document.getElementById('txt').value;
+    if (event.keyCode==13){
+        if( textoDigitado > ""){
+            var criaLista = document.createElement("li");
+            var criaText = document.createTextNode(textoDigitado);
+
+            criaLista.appendChild(criaText);
+            document.getElementById('lista').appendChild(criaLista);
+            document.getElementById('txt').value = "";
+        }
+        else {
+            alert("A tarefa não deve ser vazia!!");
+        }
     }
     else {
-        return 0
+        console.log("Tecla alternativa");
     }
 }
