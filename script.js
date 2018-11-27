6852ecee6293c0672a1fd32a5c78d31986263f6a
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2018-11-27 13:37:09 -0200

    3th commit: Grupos de Tarefas realizado (Codigo Funcional)

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ function enter() {
         if( textoDigitado > ""){
             var criaLista = document.createElement("li");
             var criaText = document.createTextNode(textoDigitado);
+            criaLista.className = 'lista';
 
+            var criaCheck = document.createElement("span");
+            var criaCheckInfo = document.createTextNode("\u2713");
+            criaCheck.className = 'checar';
+            criaCheck.appendChild(criaCheckInfo);
+            criaCheck.addEventListener("click", function(){
+                if (criaLista.className == 'lista'){
+                    criaLista.className = 'checado';
+                }
+                else if (criaLista.className == "checado") {
+                    criaLista.className = "lista";
+                }
+            })
+
+            var criaDel = document.createElement("span");
+            var criaDelInfo = document.createTextNode("\u00D7");
+            criaDel.className = 'deletar';
+            criaDel.appendChild(criaDelInfo);
+            criaDel.addEventListener("click", function(){
+               criaLista.remove();
+           })
+            criaLista.appendChild(criaCheck);
             criaLista.appendChild(criaText);
+            criaLista.appendChild(criaDel);
+
             document.getElementById('lista').appendChild(criaLista);
             document.getElementById('txt').value = "";
         }
